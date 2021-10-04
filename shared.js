375e357812f8de1b1fffa083d712049a17b0d2d8
Konrad Bieniek <konradbieniek@gmail.com>
2021-10-04 23:01:27 +0200

    adding variables

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop.style['background-image']);
 
@@ -14,6 +15,7 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
+    backdrop.style.display = "block";
     backdrop.classList.add("open");
   });
 }
@@ -34,6 +36,7 @@ function closeModal() {
   if (modal) {
     modal.classList.remove("open");
   }
+  backdrop.style.display = "none";
   backdrop.classList.remove("open");
 }
 
@@ -41,5 +44,19 @@ toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {}, 10)
   backdrop.classList.add("open");
 });
+
+ctaButton.addEventListener('animationstart', function (event){
+  console.log('Animation started', event);
+)}
+
+ctaButton.addEventListener('animationend', function (event){
+  console.log('Animation ended', event);
+)}
+
+ctaButton.addEventListener('animationiteration', function (event){
+  console.log('Animation iteration', event);
+)}
